ea5f5acbd624c299aa0e2ded5d96fb81ca364481
girmaytajebe <147225188+girmaytajebe@users.noreply.github.com>
2024-03-02 21:16:53 -0600

    first commit

---
+++ src/CommonResources/axios.js
@@ -0,0 +1,17 @@
+// import axios from "axios";
+// const instance = axios.create({
+// 	baseURL: "https://girmay-evangadiforum-backend.onrender.com/api",
+
+// 	// baseURL: "http://localhost:7700/api"
+// });
+// export default instance;
+
+
+import axios from "axios";
+const instance = axios.create({
+	
+
+	baseURL: "http://localhost:7001/api"
+
+});
+export default instance;
---
+++ src/Components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div >
+        <img src="https://www.evangadi.com/themes/humans/assets/images/misc/preloader.gif" alt="preloading" />
+    </div>
+  )
+}
+
+export default Loading
