e98c356ee4f2a02a78568dad6e4b1df71a239b31
girmaytajebe <147225188+girmaytajebe@users.noreply.github.com>
2024-03-06 21:31:16 -0600

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -23,39 +23,43 @@ function Header() {
 		navigate("/");
 	};
 	return (
-		<div className="bg-white w-full px-20 fixed top-0 left-0  shadow z-10">
-			<div className="  flex justify-between  pl-[20px] py-7 ">
-				<Link to="/home">
-					<img
-						className="max-w-none"
-						src="https://www.evangadi.com/themes/humans//assets/images/misc/evangadi-logo-home.png"
-						alt="Evangadi Logo"
-					/>
-				</Link>
-				{windowWidth > 990 ? (
-					<div className=" flex space-x-6 mr-20">
-						<Link className=" hover:text-orange-500 py-2" to="/home">
-							Home
-						</Link>
-						<Link className=" hover:text-orange-500 py-2" to="#">
-							How it Works{" "}
-						</Link>
-						<Link
-							className={`py-2 ' ${token?' hover:text-orange-500':'hover:bg-orange-500 text-white  px-20   rounded-md border border-gray-600 bg-blue-600'} `}
-							to="/sign in"
-							onClick={logingOut}
-						>
-							{!token ? "SIGN IN" : "LOG OUT"}
-						</Link>
-					</div>
-				) : (
-					<div>
-						<MiniSideMenu />
-					</div>
-				)}
-			</div>
-		</div>
-	);
+    <div className="bg-white w-full px-20  fixed top-0 left-0  shadow z-10 ">
+      <div className="  flex justify-between  pl-[20px] py-7 ">
+        <Link to="/home">
+          <img
+            className="max-w-none"
+            src="https://www.evangadi.com/themes/humans//assets/images/misc/evangadi-logo-home.png"
+            alt="Evangadi Logo"
+          />
+        </Link>
+        {windowWidth > 990 ? (
+          <div className=" flex space-x-6 mr-20">
+            <Link className=" hover:text-orange-500 py-2" to="/home">
+              Home
+            </Link>
+            <Link className=" hover:text-orange-500 py-2" to="#">
+              How it Works{" "}
+            </Link>
+            <Link
+              className={`py-2 ' ${
+                token
+                  ? " hover:text-orange-500"
+                  : "hover:bg-orange-500 text-white  px-20   rounded-md border border-gray-600 bg-blue-600"
+              } `}
+              to="/sign in"
+              onClick={logingOut}
+            >
+              {!token ? "SIGN IN" : "LOG OUT"}
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <MiniSideMenu />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,40 +1,40 @@
+
+
 import React from "react";
 import AllQuestions from "../AllQuestions/AllQuestions";
 import { Link } from "react-router-dom";
 import { useAppStateValue } from "../../App";
 
 function Home() {
-	// const [user, setUser] = useAppStateValue();
-	const username = localStorage.getItem("username");
-	return (
-		<div className="bg-slate-100  mt-[70px] pt-[50px] px-20">
-			<div className="">
-				<div className="container py-5 ">
-					<div className="ask__question flex justify-between mr-4">
-						<div>
-							<Link to="/ask-question">
-								<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ">
-									Ask Question
-								</button>
-							</Link>
-						</div>
-						<h3 className="text-2xl pr-5">
-							Welcome:
-							<span className="username text-red-400"> {username}</span>
-						</h3>
-					</div>
-					<div className="search__question pt-5 pb-3 pr-5 mr-4">
-						<input
-							type="text"
-							placeholder="search question"
-							className="w-full border border-gray-300 rounded-md p-3 "
-						/>
-					</div>
-					<AllQuestions />
-				</div>
-			</div>
-		</div>
-	);
+  const username = localStorage.getItem("username");
+
+  return (
+    <div className="bg-slate-100 mt-[70px] pt-[50px] px-4 md:px-8 lg:px-16 xl:px-20">
+      <div className="container py-5 ">
+        <div className="ask__question flex flex-col md:flex-row justify-between items-center md:mr-4">
+          <div className="mb-4 md:mb-0">
+            <Link to="/ask-question">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ">
+                Ask Question
+              </button>
+            </Link>
+          </div>
+          <h3 className="text-2xl md:text-3xl lg:text-4xl pr-0 md:pr-5">
+            Welcome:
+            <span className="username text-red-400"> {username}</span>
+          </h3>
+        </div>
+        <div className="search__question pt-5 pb-3 pr-0 md:pr-5 md:mr-4">
+          <input
+            type="text"
+            placeholder="search question"
+            className="w-full md:w-96 lg:w-120 border border-gray-300 rounded-md p-3"
+          />
+        </div>
+        <AllQuestions />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
